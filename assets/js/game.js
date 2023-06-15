fbda196f183ab5c46d720160de6de0e6b5755756
b1ndark <>
2023-06-15 22:44:01 +0000

    add questions for medium mode

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,7 +3,7 @@
  * Questions will be accessed from script.js
  */
 
-// Easy Mode
+// Easy Mode/ 10 questions in Easy mode
 
 const easyQuestions = [
     {
@@ -85,6 +85,93 @@ const easyQuestions = [
             { text: '1998', correct: false },
             { text: '1993', correct: false },
             { text: '1989', correct: true }
+        ]
+    }
+];
+
+
+// Medium Mode/ 10 questions in Medium mode
+
+const MediumQuestions = [
+    {
+        question: 'In Ancient Rome, what was a thermae?',
+        answers: [
+            { text: 'Restaurant', correct: false },
+            { text: 'Public sports', correct: false },
+            { text: 'Public baths', correct: true },
+            { text: 'Swimming Pool', correct: false }
+        ]
+    }, {
+        question: 'What was the final battle of the Napoleonic wars?',
+        answers: [
+            { text: 'The Battle of Waterloo', correct: true },
+            { text: 'The Battle for England', correct: false },
+            { text: 'The Battle of Antioch', correct: false },
+            { text: 'THe Medieval Battle of Unity Spain', correct: false }
+        ]
+    }, {
+        question: 'what name is given to the deep ditch filled with water that often surrounded castles?',
+        answers: [
+            { text: 'Lake', correct: false },
+            { text: 'Dam', correct: false },
+            { text: 'Pond', correct: false },
+            { text: 'Moat', correct: true }
+        ]
+    }, {
+        question: 'What was the name of Nazi Germany\'s official secret police?',
+        answers: [
+            { text: 'Gendarmerie', correct: false },
+            { text: 'Polizei', correct: false },
+            { text: 'Gestapo', correct: true },
+            { text: 'Carabinieri', correct: false }
+        ]
+    }, {
+        question: 'Who was the first European to reach India via The Atlantic Ocean?',
+        answers: [
+            { text: 'Vasco da Gama', correct: true },
+            { text: 'Napoleon', correct: false },
+            { text: 'Julius Caeser', correct: false },
+            { text: 'Christopher Columbus', correct: false }
+        ]
+    }, {
+        question: 'When was America discovered?',
+        answers: [
+            { text: '1257', correct: false },
+            { text: '1760', correct: false },
+            { text: '1689', correct: false },
+            { text: '1492', correct: true }
+        ]
+    }, {
+        question: 'Which US state was an independent country from 1777 to 1791?',
+        answers: [
+            { text: 'Vermont', correct: true },
+            { text: 'Texas', correct: false },
+            { text: 'California', correct: false },
+            { text: 'Florida', correct: false }
+        ]
+    }, {
+        question: 'What language was spoken in Acient Rome?',
+        answers: [
+            { text: 'Italian', correct: false },
+            { text: 'Chinese', correct: false },
+            { text: 'Latin', correct: true },
+            { text: 'French', correct: false }
+        ]
+    }, {
+        question: 'Francisco Franco ruled which European country from 1939 to 1975?',
+        answers: [
+            { text: 'Portugal', correct: false },
+            { text: 'Monaco', correct: false },
+            { text: 'Spain', correct: true },
+            { text: 'Italy', correct: false }
+        ]
+    }, {
+        question: 'What year did the Pakistan gain independence from the UK?',
+        answers: [
+            { text: '1975', correct: false },
+            { text: '1998', correct: false },
+            { text: '1945', correct: false },
+            { text: '1947', correct: true }
         ]
     }
 ];
