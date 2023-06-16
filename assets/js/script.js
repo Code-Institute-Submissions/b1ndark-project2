d0f0f30c3c1aa3ec65b8b108feab78388006c201
b1ndark <>
2023-06-16 00:25:55 +0000

    add functions from medium to hard and modiffied them

--- assets/js/script.js
+++ assets/js/script.js
@@ -354,3 +354,139 @@ function selectMediumQuiz() {
 }
 
 
+// Event Listener will open and start Hard mode quiz
+hardButton.addEventListener('click', selectHardQuiz);
+
+// This fucntion will select Hard mode and start it
+function selectHardQuiz() {
+    console.log('you have selected hard mode');
+    difficultyContainerElement.classList.add('hide');
+    questionContainerElement.classList.remove('hide');
+    currentQuestionIndex = 0;
+    score = 0;
+    showHardQuestion()
+
+
+    /**
+ * This function will reset answers from previous questions
+ * 
+ */
+    function resetHardState() {
+        while (answerButtons.firstChild) {
+            answerButtons.removeChild(answerButtons.firstChild);
+            console.log("previous answers cleared");
+        }
+    }
+
+
+    /**
+     * This function will show questions and its answers
+     */
+
+    function showHardQuestion() {
+        resetHardState();
+        console.log("show hard question");
+        /**
+         * This function will show current question
+         * Data for the questions will be collected from game.js file
+         */
+        let currentHardQuestion = hardQuestions[currentQuestionIndex];
+        questionElement.innerHTML = currentHardQuestion.question;
+        /**
+         * This function is to show answers of the current question
+         * It will add a button for each answer of the current question, in this case 4 answers
+         * Data for the answers will be collected from game.js file
+         */
+        currentHardQuestion.answers.forEach(answer => {
+            console.log("answers displayed");
+            const answerButton = document.createElement("button");
+            answerButton.innerHTML = answer.text;
+            answerButton.classList.add("btn");
+            answerButtons.appendChild(answerButton);
+            if (answer.correct) {
+                answerButton.dataset.correct = answer.correct;
+            }
+            // This Event Listener is to select an answer
+            answerButton.addEventListener('click', selectHardAnswer);
+        });
+    };
+
+
+    /**
+     * This function will activate as soon as the User selects an answer
+     */
+    function selectHardAnswer(event) {
+        console.log("answer selected");
+        const selectedAnswerButton = event.target;
+        const correctAnswer = selectedAnswerButton.dataset.correct === "true";
+        /**
+         * The answer will be checked whether is correct or wrong
+         * Also class has been added to decorate/style the correct and wrong answers
+         **/
+        if (correctAnswer) {
+            console.log("its correct");
+            selectedAnswerButton.classList.add("correct-answer");
+            score++;
+        } else {
+            console.log("its wrong");
+            selectedAnswerButton.classList.add("wrong-answer");
+        }
+        // Soon as the answer is selected whether is correct or wrong, all answers will be locked.
+        Array.from(answerButtons.children).forEach(button => {
+            if (button.dataset.correct === "true") {
+                button.classList.add("correct-answer");
+            }
+            button.disabled = true;
+            console.log("answers locked");
+        });
+        // Once the answer is selected and locked, the Next button will be displayed
+        nextQuestionButton.style.display = "block";
+    }
+
+
+    /**
+     * This function will show the user score at the end of the quiz.
+     * A text message has been added to congratulate the user.
+     */
+    function showHardScore() {
+        resetHardState()
+        questionElement.innerHTML = `Well done in completing the quiz!` +
+            `<br> You have scored ${score} out of ${hardQuestions.length} questions!`;
+        // This will hide the next question button
+        nextQuestionButton.style.display = 'none';
+        // This will display Main Menu button
+        backToIndexButton.style.display = 'block';
+    }
+
+
+    /**
+     * This function adds next question so the user can carry on with the quiz
+     * Next question Data will be loaded from game.js file
+     */
+    function handleNextHardQuestionButton() {
+        // This will hide the next question button
+        nextQuestionButton.style.display = 'none';
+        currentQuestionIndex++;
+        if (currentQuestionIndex < hardQuestions.length) {
+            showHardQuestion();
+            console.log("next question shown");
+        } else {
+            showHardScore();
+        }
+    }
+
+
+    /**
+     * This Event Listener is to active the next button
+     * By pressing it you will be taken to next question
+     */
+    nextQuestionButton.addEventListener('click', () => {
+        if (currentQuestionIndex < hardQuestions.length) {
+            console.log("next button pressed");
+            handleNextHardQuestionButton();
+        } else {
+            selectHardQuiz();
+        }
+    });
+
+}
